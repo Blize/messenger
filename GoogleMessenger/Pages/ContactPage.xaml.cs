7ef8d5d9e577d32f114fa194d8704c8b4e97f7f9
Blize <90607489+Blize@users.noreply.github.com>
2023-01-19 10:10:24 +0100

    update(ContactPage): Dynamic inserting in xaml

--- GoogleMessenger/Pages/ContactPage.xaml.cs
+++ GoogleMessenger/Pages/ContactPage.xaml.cs
@@ -5,38 +5,39 @@ namespace GoogleMessenger.Pages;
 public partial class ContactPage : ContentPage
 {
 
-    //ALL the comments you see was an Idea of Dynamic Contact Filling. Sadly it didnt worked and I had to use hardcoded code
+    readonly IList<User> source;
+    public ObservableCollection<User> Contact { get; private set; }
 
-    // public ObservableCollection<User> Contact { get; set; } = new ObservableCollection<User>();
-    // public List<User> UserList { get; set; }
     public ContactPage()
     {
         InitializeComponent();
-      //  BindingContext = this;
+        source = new List<User>();
+        CreateUserCollection();
+
+        BindingContext = this;
 
     }
-    /*
-    protected override void OnAppearing()
+
+    private void CreateUserCollection()
     {
-        UserList = new List<User>()
-        {
-            new User() {Username = "Annika", Email = "annika@mantsch.com", LastMessage = "Super! machen wir so...", ImageSource = "dotnet_bot.jpg"},
-            new User() {Username = "Jan", Email = "jan@mantsch.com", LastMessage = "Klar, bin in 10min da...", ImageSource = "dotnet_bot.jpg.jpg"},
+        source.Add(new User() { Username = "Annika", Email = "annika@something.com", LastMessage = "Klar! Freut mich...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Jan", Email = "jan@something.com", LastMessage = "Okay. Bin in 10min da...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Joel", Email = "joel@something.com", LastMessage = "Lol hahahah...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Julian", Email = "julian@something.com", LastMessage = "Bitte was??...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Peter", Email = "peter@something.com", LastMessage = "Cool! Musst du unbedingt...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Adel", Email = "adel@something.com", LastMessage = "nice hahaha, wm gad?...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Tabea", Email = "tabea@something.com", LastMessage = "Habe essen mitgebracht...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Yan", Email = "yan@something.com", LastMessage = "bi gad am volleyball spiele", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Robin", Email = "robin@something.com", LastMessage = "Jo bi gad are home", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Noel", Email = "noel@something.com", LastMessage = "ok chöi mr mache...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Gwendolin", Email = "gwendolin@something.com", LastMessage = "Bin gerade im Museum...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Zocken", Email = "null", LastMessage = "Komme in 5min on", ImageSource = "dotnet_bot.png", GroupChat = true });
+        source.Add(new User() { Username = "Sarina", Email = "sarina@something.com", LastMessage = "merci!...", ImageSource = "dotnet_bot.png", GroupChat = false });
+        source.Add(new User() { Username = "Timo", Email = "timo@something.com", LastMessage = "sorry bi ide ferie", ImageSource = "dotnet_bot.png", GroupChat = false });
 
-        };
-
-
-        if (!UserList.Any())
-        {
-            foreach (var user in UserList)
-            {
-                var userCopy = user;
-                Contact.Add(userCopy);
-            }
-        }
-        base.OnAppearing();
+        Contact = new ObservableCollection<User>(source);
     }
-    */
+
 
     async void GoToChatPage_Clicked(System.Object sender, System.EventArgs e)
     {
--- GoogleMessenger/Pages/User.cs
+++ GoogleMessenger/Pages/User.cs
@@ -7,6 +7,7 @@ namespace GoogleMessenger.Pages
         public string Email { get; set; }
         public string LastMessage { get; set; }
         public string ImageSource { get; set; }
+        public bool GroupChat { get; set; }
     }
 }
 
